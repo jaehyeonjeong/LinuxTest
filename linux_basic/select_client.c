2bf0ef06c6b2acc1f7f88d5e880bd18c68580586
definejae234@gmail.com <definejae234@gmail.com>
2022-11-23 15:47:14 +0900

    2022-11-23 HTTP

--- linux_basic/select_client.c
+++ linux_basic/select_client.c
@@ -3,124 +3,55 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <sys/time.h>
-#include <netdb.h>
-#include <stdlib.h>
 
 #define TCP_PORT 4900
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
+	struct sockaddr_in servaddr;
+	fd_set read_fd;
 	int ssock;
-	struct sockaddr_in connect_addr;
 	char mesg[BUFSIZ];
-	fd_set readfd;
-    int connect_fd;
-	int msgsize;
 
-	if(argc < 2)
-	{
-		printf("Usage : %s IP_ADDRESS\n", argv[0]);
-		return -1;
-	}
-	
-
-	/*if((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
+	if((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		perror("socket()");
 		return -1;
-	}*/
-
-	connect_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if(connect_fd == -1){
-		perror("socket Connecting Error\n");
-		return -1;
 	}
-	
-	connect_addr.sin_family = AF_INET;
-	connect_addr.sin_port = htons(atoi(argv[2]));
-	connect_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	memset(&(connect_addr.sin_zero), 0, 8);
 
-
-
-	fgets(mesg, BUFSIZ, stdin);
-
-	/*memset(&servaddr, 0, sizeof(servaddr));
+	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-
 	inet_pton(AF_INET, argv[1], &(servaddr.sin_addr.s_addr));
-	servaddr.sin_port = htons(TCP_PORT);*/
-	if(connect(connect_fd, (struct sockaddr*)&connect_addr, sizeof(connect_addr)) == -1){
-		perror("connecting Error\n");
-		return -1;
-	}
+	servaddr.sin_port = htons(TCP_PORT);
 
-	FD_ZERO(&readfd);
-	while(1){
-		FD_SET(0, &readfd);
-		FD_SET(connect_fd, &readfd);
-
-		select(connect_fd + 1, &readfd, NULL, NULL, NULL);
-
-
-		shutdown(ssock, SHUT_WR);			//쓰기 소켓을 닫음
-		if(FD_ISSET(connect_fd, &readfd)){
-			msgsize = read(connect_fd, mesg, sizeof(mesg));
-			if(msgsize <= 0) continue;
-			printf("Server Message>>%s", mesg);
-		}
-
-		if(FD_ISSET(0, &readfd)){
-			memset(mesg, '\0', sizeof(mesg));
-			fgets(mesg, 1024, stdin);
-			msgsize = strlen(mesg);
-			write(connect_fd, mesg, msgsize);
-		}
-		
-
-		shutdown(ssock, SHUT_RD);
-		close(ssock);
-	}
-	/*if(connect(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-	{
-		perror("connect()");
-		return -1;
-	}*/
-	/*if(connect_fd == 0){
+	if(connect(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
 		perror("connect()");
 		return -1;
 	}
 
-	fgets(mesg, BUFSIZ, stdin);*/
-	/*if(send(ssock, mesg, BUFSIZ, MSG_DONTWAIT) <= 0)
-	{
-		perror("send()");
-		return -1;
-	}*/
+	do{
+		FD_ZERO(&read_fd);
+		FD_SET(0, &read_fd);
+		FD_SET(ssock, &read_fd);
 
-	/*if(connect_fd == 1){
-		perror("send()");
-		return -1;
-	}
-	
-	shutdown(ssock, SHUT_WR);			//쓰기 소켓을 닫음
-	
-	memset(mesg, 0, BUFSIZ);*/
-	/*if(recv(ssock, mesg, BUFSIZ, 0) <= 0)
-	{										//데이터를 소켓으로부터 읽음
-		perror("recv()");
-		return -1;
-	}*/
-	/*if(connect_fd == 2)
-	{
-		perror("recv()");
-		return -1;
-	}
+		select(ssock + 1, &read_fd, NULL, NULL, NULL);
+		memset(mesg, 0, BUFSIZ);
 
-	
-	shutdown(ssock, SHUT_RD);
-	close(ssock);*/
+		if(FD_ISSET(ssock, &read_fd) != 0){
+			int n = read(ssock, mesg, BUFSIZ);
+			if(n <= 0) break;
+			printf("FROM SERVER : %s\n", mesg);
+			//FD_CLR(ssock, &read_fd);
+		}
+
+		if(FD_ISSET(0, &read_fd) != 0){
+			read(0, mesg, BUFSIZ);
+			int n = strlen(mesg);
+			write(ssock, mesg, BUFSIZ);
+			//FD_CLR(0, &read_fd);
+		}
+	}while(strcmp(mesg, "q"));
+
+	close(ssock);
+
 	return 0;
 }
-		
